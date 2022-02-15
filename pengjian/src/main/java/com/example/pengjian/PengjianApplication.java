e258be9e5fc103942573cca04dfa25e4e029e2a3
pengjian <342655200@qq.com>
2022-02-15 11:49:33 +0800

    提交

---
+++ pengjian/src/main/java/com/example/pengjian/PengjianApplication.java
@@ -0,0 +1,15 @@
+package com.example.pengjian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengjianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PengjianApplication.class, args);
+    }
+
+
+
+}
